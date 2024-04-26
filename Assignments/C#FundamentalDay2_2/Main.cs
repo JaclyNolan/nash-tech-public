e70751335f4c471aee9ea7440d75397cfbedb2a5
JaclyNolan <anhbg330011@gmail.com>
2024-04-26 16:18:03 +0700

    Day2_2

---
+++ Assignments/C#FundamentalDay2_2/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_FundamentalDay2_2
+{
+	internal class Main
+	{
+		public async Task<List<int>> ListAllPrimeAsync(int start, int end)
+		{
+			List<int> primes = new List<int>();
+			//This is not at all optimized. All tasks are running on a single thread under
+			//Concurrency methods. No tasks are truly running in parallel.
+			List<Task> tasks = new List<Task>();
+			object lockObj = new object();
+			for (int i = start;  i <= end; i++)
+			{
+				tasks.Add(IsPrimeAsync(i, primes, lockObj));
+			}
+
+			await Task.WhenAll(tasks);
+
+			return primes;
+		}
+		private static async Task IsPrimeAsync(int x, List<int> primes, object lockObj)
+		{
+			if (x <= 1) return;
+
+			for (int i = 2; i <= Math.Sqrt(x); i++)
+			{
+				if (x % i == 0) return;
+			}
+			//Simulating long task
+			await Task.Delay(1000);
+			lock(lockObj) {
+				primes.Add(x);
+			}
+		}
+	}
+}
---
+++ Assignments/C#FundamentalDay2_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using C_FundamentalDay2_2;
+do
+{
+	Console.WriteLine("Enter start and end to find primes between them! (start < end)");
+	Console.Write("start = ");
+	int start;
+	while (!int.TryParse(Console.ReadLine(), out start))
+	{
+		Console.WriteLine("Invalid input!");
+		Console.Write("start = ");
+	}
+
+	Console.Write("end = ");
+	int end;
+	while (!int.TryParse(Console.ReadLine(), out end))
+	{
+		Console.WriteLine("Invalid input!");
+		Console.Write("end = ");
+	}
+
+	Main main = new Main();
+	List<int> primes = await main.ListAllPrimeAsync(start, end);
+	Console.WriteLine(primes.Count);
+
+	//foreach (int prime in primes)
+	//{
+	//	Console.Write($"{prime} ");
+	//}
+	Console.WriteLine();
+} while (true);
