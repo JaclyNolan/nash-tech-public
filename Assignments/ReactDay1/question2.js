1119d2f5b5d372a6dabfa1adeb4d1dcb518a80c3
JaclyNolan <anhbg330011@gmail.com>
2024-05-21 17:36:41 +0700

    Initialize ReactDay2 Assignement

---
+++ Assignments/ReactDay1/question2.js
@@ -0,0 +1,8 @@
+const arr = [
+    { name: "Alex", grade: 15, point: 15 },
+    { name: "Devlin", grade: 15, point: 25 },
+    { name: "Eagle", grade: 13, point: 12 },
+    { name: "Sam", grade: 14, point: 26 },
+]
+
+arr.sort()
