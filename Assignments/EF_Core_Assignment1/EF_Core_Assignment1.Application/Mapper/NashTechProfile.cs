67c62b59be2b1d4d8125daae28368195e386f40e
JaclyNolan <anhbg330011@gmail.com>
2024-05-29 19:22:14 +0700

    Remove last project code

--- Assignments/EF_Core_Assignment1/EF_Core_Assignment1.Application/Mapper/NashTechProfile.cs
+++ Assignments/EF_Core_Assignment1/EF_Core_Assignment1.Application/Mapper/NashTechProfile.cs
@@ -1,9 +1,6 @@
 ﻿using AutoMapper;
 using EF_Core_Assignment1.Application.DTOs.Book;
 using EF_Core_Assignment1.Application.DTOs.Common;
-using EF_Core_Assignment1.Application.DTOs.Department;
-using EF_Core_Assignment1.Application.DTOs.Employee;
-using EF_Core_Assignment1.Application.DTOs.Project;
 using EF_Core_Assignment1.Domain.Entities;
 
 namespace EF_Core_Assignment1.Application.Mapper
@@ -12,27 +9,6 @@ namespace EF_Core_Assignment1.Application.Mapper
     {
         
         public NashTechProfile() {
-            // Employee
-            CreateMap<Employee, EmployeeViewModel>()
-                .ForMember(dest => dest.SalaryAmount, opt => opt.MapFrom(src => src.Salary.SalaryAmount))
-                .ForMember(dest => dest.ProjectIds, opt => opt.MapFrom(src => src.ProjectEmployees.Select(pe => pe.ProjectId)));
-            CreateMap<EmployeeCreateRequest, Employee>()
-                .ForMember(dest => dest.Salary, opt => opt.MapFrom(src => new Salary { SalaryAmount = src.SalaryAmount }));
-            CreateMap<EmployeeEditRequest, Employee>()
-                .ForPath(dest => dest.Salary.SalaryAmount, opt => opt.MapFrom(src => src.SalaryAmount));
-
-            // Department
-            CreateMap<Department, DepartmentViewModel>()
-                .ForMember(dest => dest.EmployeeIds, opt => opt.MapFrom(src => src.Employees.Select(e => e.Id)));
-            CreateMap<DepartmentCreateRequest, Department>();
-            CreateMap<DepartmentEditRequest, Department>();
-
-            // Project
-            CreateMap<Project, ProjectViewModel>()
-                .ForMember(dest => dest.EmployeeIds, opt => opt.MapFrom(src => src.Employees.Select(e => e.Id)));
-            CreateMap<ProjectCreateRequest, Project>();
-            CreateMap<ProjectEditRequest, Project>();
-
             // Book
             CreateMap<CreateBookRequest, Book>();
             CreateMap<UpdateBookRequest, Book>();
--- Assignments/EF_Core_Assignment1/EF_Core_Assignment1.Persistance/Seeder/NashTechSeeder.cs
+++ Assignments/EF_Core_Assignment1/EF_Core_Assignment1.Persistance/Seeder/NashTechSeeder.cs
@@ -11,76 +11,12 @@ namespace EF_Core_Assignment1.Persistance.Seeder
         //private readonly UserManager<ApplicationUser> _userManager;
         public static void Seed(this NashTechContext _context)
         {
-            if (_context.Departments.Any() || _context.Employees.Any() || _context.Projects.Any() || _context.Salaries.Any() || _context.Books.Any())
+            if (_context.Books.Any())
             {
                 Console.WriteLine("Database already seeded");
                 // Data already seeded
                 return;
             }
-
-            // Seed Departments
-            List<Department> departments = new List<Department>
-            {
-                new Department { Name = "Software Development" },
-                new Department { Name = "Finance" },
-                new Department { Name = "Accountant" },
-                new Department { Name = "HR" },
-            };
-            _context.Departments.AddRange(departments);
-            _context.SaveChanges();
-
-            // Seed Employees
-            var employeeFaker = new Faker<Employee>()
-                .RuleFor(e => e.Name, f => f.Person.FullName)
-                .RuleFor(e => e.JoinedDate, f => f.Date.Between(DateTime.Parse("1/1/2020"), DateTime.Now))
-                .RuleFor(e => e.DepartmentId, f => f.PickRandom(departments).Id);
-
-            var employees = employeeFaker.Generate(20); // Generate 20 employees
-
-            // Seed Salaries
-            var salaryFaker = new Faker<Salary>()
-                .RuleFor(s => s.SalaryAmount, f => (float)f.Finance.Amount(2000, 8000));
-
-            foreach (var employee in employees)
-            {
-                employee.Salary = salaryFaker.Generate(); // Generate a salary for each employee
-            }
-            _context.Employees.AddRange(employees);
-            _context.SaveChanges();
-
-            // Seed Projects
-            var projectFaker = new Faker<Project>()
-                .RuleFor(p => p.Name, f => f.Commerce.ProductName());
-
-            var projects = projectFaker.Generate(10); // Generate 10 projects
-            _context.Projects.AddRange(projects);
-            _context.SaveChanges();
-
-            // Seed ProjectEmployees
-            var projectEmployees = new List<ProjectEmployee>();
-            var random = new Random();
-            foreach (var employee in employees)
-            {
-                var numberOfProjects = random.Next(1, 4); // Random number between 1 and 2
-
-                // Shuffle the projects list
-                var shuffledProjects = projects.OrderBy(p => random.Next()).ToList();
-
-                for (int i = 0; i < numberOfProjects; i++)
-                {
-                    var projectEmployee = new ProjectEmployee
-                    {
-                        ProjectId = shuffledProjects[i].Id,
-                        EmployeeId = employee.Id,
-                        Enable = new Faker().Random.Bool()
-                    };
-
-                    projectEmployees.Add(projectEmployee);
-                }
-            }
-
-            _context.ProjectEmployees.AddRange(projectEmployees);
-            _context.SaveChanges();
 
             // Seed Book
             var bookFaker = new Faker<Book>()
